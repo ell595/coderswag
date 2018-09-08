d0744a745707d9b67e049e6d38be21dd39facf14
ell595 <e.fraser3@gmail.com>
2018-09-08 23:19:46 +0100

    finished functionality

---
+++ coderswag/coderswag/Model/Product.swift
@@ -0,0 +1,21 @@
+//
+//  Product.swift
+//  coderswag
+//
+//  Created by Elliot Fraser on 08/09/2018.
+//  Copyright © 2018 Elliot Fraser. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+}
---
+++ coderswag/coderswag/View/ProductCell.swift
@@ -0,0 +1,21 @@
+//
+//  ProductCell.swift
+//  coderswag
+//
+//  Created by Elliot Fraser on 08/09/2018.
+//  Copyright © 2018 Elliot Fraser. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    func updateViews(product: Product) {
+        productImage.image = UIImage(named: product.imageName)
+        productTitle.text = product.title
+        productPrice.text = product.price
+    }
+}
